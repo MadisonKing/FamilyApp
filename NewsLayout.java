b891238ec753af84ef96f3914cf2c32c25c41231
HeathWil <Heather.o.wilson@ou.edu>
2017-05-02 23:49:09 -0500

    Blog window changes

--- NewsLayout.java
+++ NewsLayout.java
@@ -1,17 +1,24 @@
 package com.familyApp.FamilyApp;
 
+import java.util.ArrayList;
+
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 public class NewsLayout extends VerticalLayout {
 
-	
-	protected News news;
-	
+		
 	protected User user;
+	
+	ArrayList<News> posts= new ArrayList<News>();
+	
+	protected TextField text= new TextField("Enter post here");
 	
 	protected Button post= new Button("Post");
 	
@@ -30,7 +37,21 @@ public class NewsLayout extends VerticalLayout {
 	private void initLayout(){
 		HorizontalLayout buttons= new HorizontalLayout();
 		buttons.addComponents(post, cancel);
-		addComponents(grid, buttons);
+		addComponents(grid,text, buttons);
+		post.addClickListener(new PostBtnListener());
 	}
 	
+	
+	public class PostBtnListener implements ClickListener{
+
+		@Override
+		public void buttonClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			News news= new News(text.getValue(), user);
+			posts.add(news);
+			grid.setItems(posts);
+			
+		}
+		
+	}
 }
